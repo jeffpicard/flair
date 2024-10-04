493a7de8ac13365e998299d98031b399e2075a5e
Jeff Picard <jeff.t.picard@gmail.com>
2024-10-04 11:34:35 -0700

    fixup

--- flair/trainers/trainer.py
+++ flair/trainers/trainer.py
@@ -712,48 +712,47 @@ class ModelTrainer(Pluggable):
                     current_epoch_has_best_model_so_far = False
                     validation_scores = ()
 
-                    if is_main_process():
-                        for evaluation_split, evaluation_split_data in evaluation_splits.items():
-                            eval_result = self.model.evaluate(
-                                evaluation_split_data,
-                                out_path=base_path / f"{evaluation_split}.tsv",
-                                mini_batch_size=eval_batch_size,
-                                exclude_labels=exclude_labels,
-                                main_evaluation_metric=main_evaluation_metric,
-                                gold_label_dictionary=gold_label_dictionary_for_eval,
-                                embedding_storage_mode=embeddings_storage_mode,
-                                gold_label_type=self.model.label_type,
-                                gold_label_dictionary_for_eval=gold_label_dictionary_for_eval,
-                            )
+                    for evaluation_split, evaluation_split_data in evaluation_splits.items():
+                        eval_result = self.model.evaluate(
+                            evaluation_split_data,
+                            out_path=base_path / f"{evaluation_split}.tsv",
+                            mini_batch_size=eval_batch_size,
+                            exclude_labels=exclude_labels,
+                            main_evaluation_metric=main_evaluation_metric,
+                            gold_label_dictionary=gold_label_dictionary_for_eval,
+                            embedding_storage_mode=embeddings_storage_mode,
+                            gold_label_type=self.model.label_type,
+                            gold_label_dictionary_for_eval=gold_label_dictionary_for_eval,
+                        )
 
-                            # log results
-                            log.info(
-                                f"{evaluation_split.upper()} : loss {eval_result.loss}"
-                                f" - {main_evaluation_metric[1]}"
-                                f" ({main_evaluation_metric[0]})"
-                                f"  {round(eval_result.main_score, 4)}"
-                            )
+                        # log results
+                        log.info(
+                            f"{evaluation_split.upper()} : loss {eval_result.loss}"
+                            f" - {main_evaluation_metric[1]}"
+                            f" ({main_evaluation_metric[0]})"
+                            f"  {round(eval_result.main_score, 4)}"
+                        )
 
-                            # depending on memory mode, embeddings are moved to CPU, GPU or deleted
-                            store_embeddings(evaluation_split_data, embeddings_storage_mode)
+                        # depending on memory mode, embeddings are moved to CPU, GPU or deleted
+                        store_embeddings(evaluation_split_data, embeddings_storage_mode)
 
-                            self._publish_eval_result(eval_result, evaluation_split, global_step=epoch)
+                        self._publish_eval_result(eval_result, evaluation_split, global_step=epoch)
 
-                            # use DEV split to determine if this is the best model so far
-                            if determine_best_epoch_using_dev_score and evaluation_split == "dev":
-                                validation_scores = eval_result.main_score, eval_result.loss
+                        # use DEV split to determine if this is the best model so far
+                        if determine_best_epoch_using_dev_score and evaluation_split == "dev":
+                            validation_scores = eval_result.main_score, eval_result.loss
 
-                                if eval_result.main_score > best_epoch_score:
-                                    current_epoch_has_best_model_so_far = True
-                                    best_epoch_score = eval_result.main_score
-
-                        # if not using DEV score, determine best model using train loss
-                        if not determine_best_epoch_using_dev_score:
-                            validation_scores = (train_loss,)
-
-                            if epoch_train_loss < best_epoch_score:
+                            if eval_result.main_score > best_epoch_score:
                                 current_epoch_has_best_model_so_far = True
-                                best_epoch_score = train_loss
+                                best_epoch_score = eval_result.main_score
+
+                    # if not using DEV score, determine best model using train loss
+                    if not determine_best_epoch_using_dev_score:
+                        validation_scores = (train_loss,)
+
+                        if epoch_train_loss < best_epoch_score:
+                            current_epoch_has_best_model_so_far = True
+                            best_epoch_score = train_loss
 
                     # - LossFilePlugin -> somehow prints all relevant metrics
                     # - AnnealPlugin -> scheduler step
