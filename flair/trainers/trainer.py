eb6f5b056b50f818e2b16f49df6c37fc2cf676f9
Jeff Picard <jeff.t.picard@gmail.com>
2024-10-28 18:11:08 -0700

    GH-3429: Rename parameter group

--- flair/trainers/trainer.py
+++ flair/trainers/trainer.py
@@ -167,7 +167,7 @@ class ModelTrainer(Pluggable):
         create_file_logs: bool = True,
         create_loss_file: bool = True,
         write_weights: bool = False,
-        # scaling
+        # acceleration
         multi_gpu: bool = False,
         # plugins
         plugins: Optional[List[TrainerPlugin]] = None,
@@ -243,7 +243,7 @@ class ModelTrainer(Pluggable):
         create_file_logs: bool = True,
         create_loss_file: bool = True,
         write_weights: bool = False,
-        # scaling
+        # acceleration
         use_amp: bool = False,
         multi_gpu: bool = False,
         # plugins
@@ -294,7 +294,7 @@ class ModelTrainer(Pluggable):
             create_file_logs=create_file_logs,
             create_loss_file=create_loss_file,
             write_weights=write_weights,
-            # scaling
+            # acceleration
             use_amp=use_amp,
             multi_gpu=multi_gpu,
             # plugins
@@ -339,7 +339,7 @@ class ModelTrainer(Pluggable):
         create_file_logs: bool = True,
         create_loss_file: bool = True,
         write_weights: bool = False,
-        # scaling
+        # acceleration
         use_amp: bool = False,
         multi_gpu: bool = False,
         # plugins
